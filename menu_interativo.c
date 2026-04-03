20976449819ec5a0075af8958330f9f7e765a193
joaoPSHub <joaopaulosoaresmms@gmail.com>
2026-04-03 13:11:37 +0000

    codigo_inicial_01

--- menu_interativo.c
+++ menu_interativo.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main(){
+    int opcao;
+    int numeroSecreto, palpite;
+
+    printf("Menu Principal \n");
+    printf("1. Iniciar Jogo \n");
+    printf("2. Ver regras \n");
+    printf("3. Sair \n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        srand(time(0));
+        numeroSecreto = rand() % 10;
+        printf("Digite um número de 0 a 9: ");
+        scanf("%d", &palpite);
+        if (numeroSecreto == palpite)
+        {
+            printf("Você acertou! \n");
+            printf("Numero Secreto %d\n", numeroSecreto);
+        } else {
+        
+            printf("Você errou! \n");
+            printf("Numero Secreto %d\n", numeroSecreto);
+        }
+        break;
+    case 2:
+        printf("As regras são... \n");
+        break;
+    case 3:
+        printf("Saindo do jogo \n");
+        break;
+    default:
+        printf("Opção inválida! \n");
+        break;
+    }
+
+
+return 0;
+
+}
